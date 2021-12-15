63d4e325ec47bf7590e9897b29b211273ca3e129
Anastasiia Kungurova <kngrvs@yandex.ru>
2021-12-15 16:48:23 +0300

    master

--- task_2/solution.js
+++ task_2/solution.js
@@ -3,7 +3,7 @@ function calcShipping(sum, min, shipping) {
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
 
-    let shippingSum = 0; // создайте переменную shippingSum
+    let shippingSum; // создайте переменную shippingSum
     if(productsSum == 0 || productsSum >= freeShippingMinSum){ 
         shippingSum = 0; 
     } else ((0 < productsSum) && (productsSum < freeShippingMinSum)); // если productsSum больше 0 и меньше freeShippingMinSum,
