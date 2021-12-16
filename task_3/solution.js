739c07cbe1e2c876b314fe12ef4d83cd6d7104cc
Anastasiia Kungurova <kngrvs@yandex.ru>
2021-12-16 12:59:10 +0300

    master

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,13 +2,17 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
     // title - наименование
     // price - цена
     // count - количество
+    
+    let itemCountText = "";
+    itemCountText = (itemCountText + product.count + ` x ` + product.price + ` ₽ = `);
+    let sum = product.count * product.price;
+    itemCountText = (itemCountText + sum + ` ₽`);
 
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
