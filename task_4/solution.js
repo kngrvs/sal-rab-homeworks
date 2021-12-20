deea2a81fe3e57389fab954697b0228becf69d5e
Anastasiia Kungurova <kngrvs@yandex.ru>
2021-12-20 15:36:56 +0300

    master

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,8 +2,8 @@
 function parseProducts(json) {
     let data = JSON.parse(json);
     let products = data.products;
-    return products;
 }
+return parseProducts;
 
 
 function renderProductsCards(json){
