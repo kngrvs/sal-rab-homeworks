1ebc4a8937fcf45d33e15de708b668a6132aea02
Anastasiia Kungurova <kngrvs@yandex.ru>
2021-12-22 10:29:12 +0300

    master

--- task_final/solution.js
+++ task_final/solution.js
@@ -48,8 +48,12 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i+=1) {  
-        data.goods[i]={title: goods[i].title,count: goods[i].count};
+        data.goods[i]={
+            title: goods[i].title, 
+            count: goods[i].count,
+        };
     }
+    
     data.order.address = address;
     data.order.sum = name + phone + address + goods + sum;
     
