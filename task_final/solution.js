a0a18c7979ea4b7909bb4ff8512fec163cc7133b
Anastasiia Kungurova <kngrvs@yandex.ru>
2021-12-22 13:09:33 +0300

    master

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,7 +39,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = {
         client: name + ' ' + phone, 
         order: {
-            address: street + house + entrance + floor + flat,
+            address: [street + house + entrance + floor + flat],
             sum,
         }, 
         goods: [],
